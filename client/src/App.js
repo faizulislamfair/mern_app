d1e84d9f26ac7f9e611a5ee653623e9e98d429dc
faizulislamfair <faizulislamfair@gmail.com>
2022-06-25 02:15:47 +0600

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+import Axios from 'axios'
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([])
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState("")
+
+
+  useEffect(() => {
+      Axios.get("http://localhost:3001/getUsers").then((response) => {
+       setListOfUsers(response.data)
+      })
+  }, [])  
+
+
+  const createUser = () => {
+     Axios.post("http://localhost:3001/createUser", {
+        name: name,
+        age: age,
+        username: username,
+      }).then((response) => {
+      setListOfUsers([...listOfUsers, {name: name, age: age, username: username}])
+     })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="usersDisplay">
+          {listOfUsers.map((user) => {
+            return (
+              <div>
+                <h1>Name: {user.name}</h1>
+                <h1>Name: {user.age}</h1>
+                <h1>Name: {user.username}</h1>
+              </div>
+            );
+          })}
+      </div>
+
+
+      <div>
+         <input type="text" placeholder="Name" onChange={(event) => {setName(event.target.value);}} />
+         <input type="number" placeholder="Age" onChange={(event) => {setAge(event.target.value);}} />
+         <input type="text" placeholder="Username" onChange={(event) => {setUsername(event.target.value);}} />
+         <button onClick={createUser}>Create User</button>
+      </div>
     </div>
   );
 }
